bda8f8b659538ead808308691567c1ca14ba74e0
isinnur <isinngnyy@gmail.com>
2023-12-26 17:45:20 +0300

    feat: add price format

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { Product } from "@prisma/client";
 import Link from "next/link";
+import PriceTag from "./PriceTag";
+import Image from "next/image";
 
 interface ProductCardProps {
     product: Product
@@ -7,8 +9,18 @@ interface ProductCardProps {
 export default function ProductCard({product} : ProductCardProps){
     return(
         <Link href={"/products"+ product.id} className="card w-full bg-base-100 hover:shadow-xl transition-shadow">
+           <figure>
+            <Image 
+                src={product.imageUrl}
+                alt={product.name}
+                width={800}
+                height={400}
+            />
+           </figure>
             <div className="card-body">
-
+                <h2 className="card-title"> {product.name} </h2>
+                <p> {product.description} </p>
+                <PriceTag price={product.price}/>
             </div>
         </Link> 
     )
