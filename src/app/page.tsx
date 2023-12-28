acf3395509a76f22607346e77472cf196fa75f54
isinnur <isinngnyy@gmail.com>
2023-12-28 10:14:42 +0300

    feat: add list of all products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ProductCard from '@/components/ProductCard'
 import { prisma } from '@/lib/db/prisma'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home() {
   const products = await prisma.product.findMany({
@@ -10,7 +11,32 @@ export default async function Home() {
   })
   return (
    <div>
-    <ProductCard product={products[0]}/>
+    <div className='hero rounded-xl bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row'>
+      <Image src={products[0].imageUrl} 
+      alt={products[0].name} 
+      width={400} height={400}
+      className='w-full max-w-sm rounded-lg shadow-2xl'
+      priority
+      />
+      <div>
+        <h1 className='text-5xl font-bold'>
+          {products[0].name}
+        </h1>
+        <p className='py-6'> {products[0].description} </p>
+        <Link href={"/products/" + products[0].id}
+        className='btn btn-primary'
+        >
+          Check it out
+        </Link>
+      </div>
+      </div>
+    </div>
+    <div className='my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
+      {products.slice(1).map(product =>(
+        <ProductCard product={product} key={product.id}/>
+      ))}
+    </div>
    </div>
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,6 +7,7 @@ interface ProductCardProps {
     product: Product
 }
 export default function ProductCard({product} : ProductCardProps){
+   const isNew = Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 * 24 * 7;
     return(
         <Link href={"/products"+ product.id} className="card w-full bg-base-100 hover:shadow-xl transition-shadow">
            <figure>
@@ -15,10 +16,14 @@ export default function ProductCard({product} : ProductCardProps){
                 alt={product.name}
                 width={800}
                 height={400}
+                className="h-48 object-cover"
             />
            </figure>
             <div className="card-body">
-                <h2 className="card-title"> {product.name} </h2>
+                    <h2 className="card-title">
+                     {product.name}
+                     </h2>
+                     {isNew && <div className="badge badge-secondary">NEW</div>}
                 <p> {product.description} </p>
                 <PriceTag price={product.price}/>
             </div>
