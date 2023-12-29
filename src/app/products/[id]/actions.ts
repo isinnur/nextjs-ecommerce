ee33839a9c55182c4194967e38e09e58af5cced9
isinnur <isinngnyy@gmail.com>
2023-12-29 13:14:27 +0300

    feat: add create cart

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -1,5 +1,21 @@
 "use server";
+import {createCart, getCart } from "@/lib/db/cart";
+import { prisma } from "@/lib/db/prisma";
 
 export async function incrementProductQuantity(productId:string) {
-    
+    const cart = (await getCart() ) ?? ( await createCart());
+    const articleInCart = cart.items.find(item => item.productId === productId)
+
+    if(articleInCart){
+        await prisma.cartItem.update({
+            where: {id: articleInCart.id},
+            data: {quantity: {increment: 1}}
+        })
+    } else{
+        await prisma.cartItem.create({
+            data: {
+                cartId: cart.id
+            }
+        })
+    }
 }
--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,7 +1,48 @@
+import { cookies } from "next/dist/client/components/headers";
 import { prisma } from "./prisma";
+import { Cart, Prisma } from "@prisma/client";
 
-export async function crateCart() {
+export type CartWithProducts = Prisma.CartGetPayload<{
+    include: {items: {include : {product: true}}}
+}>
+
+export type ShoppingCart = CartWithProducts & {
+    size: number,
+    subtotal: number,
+};
+
+export async function getCart(): Promise<ShoppingCart | null> {
+    const localCartId = cookies().get("localCartId")?.value
+    const cart = localCartId ? 
+    await prisma.cart.findUnique({
+        where: {id: localCartId},
+        include: {items: {include: {product: true}}}
+    })
+    : null;
+    if(!cart) {
+        return null;
+    }
+
+    return {
+        ...cart,
+        size: cart.items.reduce((acc, item) => acc + item.quantity,0),
+        subtotal : cart.items.reduce(
+            (acc,item) => acc + item.quantity * item.product.price,0
+        )
+    }
+}
+
+export async function createCart() : Promise <ShoppingCart> {
     const newCart = await prisma.cart.create({
         data: {}
     })
+    cookies().set("localCartId", newCart.id);
+
+    return {
+        ...newCart,
+        items: [],
+        size: 0,
+        subtotal: 0
+    }
+
 }
