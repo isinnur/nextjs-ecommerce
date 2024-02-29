7715bee6a77201c762b0e310eab0781ef53eb7a2
isinnur <isinngnyy@gmail.com>
2024-02-29 23:54:01 +0300

    feat: add search input

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png"
+
 export default function Navbar(){
  return(
     <div className="bg-base-100">
-      <div className="navbar max-w-7xl m-auto flex-col"></div>
+      <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
+         <div className="flex-1">
+            <Link href="/" className="btn btn-ghost text-xl normal-case">
+               <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
+               Flowmazon
+            </Link>
+         </div>
+         <div className="flex-none gap-2">
+            <form>
+               <div className="form-control">
+                  <input name="searchQuery" placeholder="Search" className="input input-bordered w-full min-w-[100px]" />
+               </div>
+            </form>
+         </div>
+      </div>
     </div>
  )   
 }
