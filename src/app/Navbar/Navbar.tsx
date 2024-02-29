fc9fb36dd27e5b91ebbb36f276a58b31a1e61a23
isinnur <isinngnyy@gmail.com>
2024-03-01 00:43:09 +0300

    feat: add shopping cart

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png"
 import { redirect } from "next/navigation";
+import ShoppingCartButton from "@/app/Navbar/ShoppingCartButton";
+import { getCart } from "@/lib/db/cart";
 
 async function searchProducts(formData: FormData) {
    "use server";
@@ -13,7 +15,8 @@ async function searchProducts(formData: FormData) {
    
 }
 
-export default function Navbar(){
+export default async function Navbar(){
+   const cart = await getCart();
  return(
     <div className="bg-base-100">
       <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
@@ -29,6 +32,7 @@ export default function Navbar(){
                   <input name="searchQuery" placeholder="Search" className="input input-bordered w-full min-w-[100px]" />
                </div>
             </form>
+            <ShoppingCartButton cart={cart}/>
          </div>
       </div>
     </div>
