1adb1638e3c1314e32c4fbfd93a83688bd5b8232
isinnur <isinngnyy@gmail.com>
2024-03-11 00:52:12 +0300

    deploy test

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,34 @@
-"use server";
-
-import { createCart, getCart } from "@/lib/db/cart";
+import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
-import { revalidatePath } from "next/cache";
+import { env } from "@/lib/env";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
-export async function incrementProductQuantity(productId: string) {
-  const cart = (await getCart()) ?? (await createCart());
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user }) {
+      await mergeAnonymousCartIntoUserCart(user.id);
+    },
+  },
+};
 
-  const articleInCart = cart.items.find((item) => item.productId === productId);
+const handler = NextAuth(authOptions);
 
-  if (articleInCart) {
-    await prisma.cart.update({
-      where: { id: cart.id },
-      data: {
-        items: {
-          update: {
-            where: { id: articleInCart.id },
-            data: { quantity: { increment: 1 } },
-          },
-        },
-      },
-    });
-  } else {
-    await prisma.cart.update({
-      where: { id: cart.id },
-      data: {
-        items: {
-          create: {
-            productId,
-            quantity: 1,
-          },
-        },
-      },
-    });
-  }
-
-  revalidatePath("/products/[id]");
-}
+export { handler as GET, handler as POST };
